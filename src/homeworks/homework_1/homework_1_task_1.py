fc3ed027a41efbdaf2c504e7e51b9ee526e3b5b6
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2024-03-22 02:52:55 +0300

    fix: rework Registry with Generic

--- src/homeworks/homework_1/homework_1_task_1.py
+++ src/homeworks/homework_1/homework_1_task_1.py
@@ -1,27 +1,34 @@
-from typing import Callable, Dict, Optional
+from collections import Counter, OrderedDict, defaultdict
+from typing import Callable, Dict, Generic, Optional, Type, TypeVar
+
+ParentClass = TypeVar("ParentClass")
+
+USER_TEXT = (
+    "Registry with ParentClass 'dict', default=dict and Elements:\n"
+    "\t1)Counter\n"
+    "\t2)OrderedDict\n"
+    "\t3)defaultdict\n"
+    "Enter class name: "
+)
 
 
-class Registry:
+class Registry(Generic[ParentClass]):
     def __init__(self, default: Optional[type] = None) -> None:
         self.default = default
-        self.classes: Dict[str, type] = dict()
+        self.classes: Dict[str, Type[ParentClass]] = dict()
 
-    def __getitem__(self, item: type) -> "Registry":
-        self.base_class = item
-        return self
+    def register(self, name: str) -> Callable[[Type[ParentClass]], Type[ParentClass]]:
+        """Decorator for adding a class with a name into the register"""
 
-    def register(self, name: str) -> Callable[[type], type]:
-        """Decorator for adding a class with a name into the register, if it is a subclass of the base_class"""
-
-        def inner(registered_class: type) -> type:
-            if issubclass(registered_class, self.base_class):
-                self.classes[name] = registered_class
-
+        def inner(registered_class: Type[ParentClass]) -> Type[ParentClass]:
+            self.classes[name] = registered_class
             return registered_class
 
+        if name in self.classes:
+            raise ValueError(f"{name} already in registry")
         return inner
 
-    def dispatch(self, name: str) -> type:
+    def dispatch(self, name: str) -> Type[ParentClass] | type:
         """Get class with a name from the register"""
         if name in self.classes:
             return self.classes[name]
@@ -29,3 +36,20 @@ class Registry:
             raise ValueError(f"{name} was not registered")
         else:
             return self.default
+
+
+def main() -> None:
+    register = Registry[dict](default=dict)
+    register.register("Counter")(Counter)
+    register.register("OrderedDict")(OrderedDict)
+    register.register("defaultdict")(defaultdict)
+    user_input = input(USER_TEXT)
+    user_class = register.dispatch(user_input)
+    print("Return:", user_class)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as error:
+        print(error)
--- tests/homeworks/homework_1/test_homework_1_task_1.py
+++ tests/homeworks/homework_1/test_homework_1_task_1.py
@@ -3,54 +3,75 @@ import pytest
 from src.homeworks.homework_1.homework_1_task_1 import *
 
 
-class DummyClass:
-    pass
+class TestRegistry:
+    class DummyClass:
+        pass
 
+    registry = Registry[DummyClass](default=dict)
+    registry_no_default = Registry[DummyClass]()
+    registry_dict = Registry[dict]()
 
-registry = Registry(default=dict)[DummyClass]
-registry_no_default = Registry()[DummyClass]
+    for name, new_class in [("OrderedDict", OrderedDict), ("defaultdict", defaultdict), ("Counter", Counter)]:
+        registry_dict.register(name)(new_class)
 
+    @registry.register("SubclassDummy1")
+    @registry_no_default.register("SubclassDummy1")
+    class SubclassDummy1(DummyClass):
+        pass
 
-@registry.register("SubclassDummy1")
-@registry_no_default.register("SubclassDummy1")
-class SubclassDummy1(DummyClass):
-    pass
+    @registry.register("SubclassDummy2")
+    @registry_no_default.register("SubclassDummy2")
+    class SubclassDummy2(DummyClass):
+        pass
 
+    @pytest.mark.parametrize(
+        "name,storage",
+        [
+            ("SubclassDummy1", registry),
+            ("SubclassDummy2", registry),
+            ("OrderedDict", registry_dict),
+            ("defaultdict", registry_dict),
+            ("Counter", registry_dict),
+        ],
+    )
+    def test_registry_store_name(self, name, storage):
+        assert name in storage.classes
 
-@registry.register("SubclassDummy2")
-@registry_no_default.register("SubclassDummy2")
-class SubclassDummy2(DummyClass):
-    pass
+    @pytest.mark.parametrize("name,expected", [("SubclassDummy1", SubclassDummy1), ("SubclassDummy2", SubclassDummy2)])
+    def test_registry_store_class(self, name, expected):
+        assert self.registry.classes[name] == expected
 
+    @pytest.mark.parametrize(
+        "name,my_class,storage",
+        [
+            ("SubclassDummy1", SubclassDummy1, registry),
+            ("SubclassDummy2", SubclassDummy2, registry),
+            ("OrderedDict", OrderedDict, registry_dict),
+            ("defaultdict", defaultdict, registry_dict),
+            ("Counter", Counter, registry_dict),
+        ],
+    )
+    def test_dispatch(self, name, my_class, storage):
+        assert storage.dispatch(name) == my_class
 
-@registry.register("NotSubclass")
-@registry_no_default.register("NotSubclass")
-class NotSubclass:
-    pass
+    @pytest.mark.parametrize(
+        "name,default",
+        [
+            ("NotInRegistry_1", dict),
+            ("NotInRegistry_2", dict),
+        ],
+    )
+    def test_default_dispatch(self, name, default):
+        assert self.registry.dispatch(name) == default
 
+    @pytest.mark.parametrize("name", ["NotInRegistry", "defaultdict", "OrderedDict", "Counter"])
+    def test_dispatch_exception(self, name):
+        with pytest.raises(ValueError):
+            self.registry_no_default.dispatch("NotInRegistry")
 
-@pytest.mark.parametrize("name,expected", [("SubclassDummy1", True), ("SubclassDummy2", True), ("NotSubclass", False)])
-def test_registry_store_name(name, expected):
-    assert (name in registry.classes) == expected
-
-
-@pytest.mark.parametrize("name,expected", [("SubclassDummy1", SubclassDummy1), ("SubclassDummy2", SubclassDummy2)])
-def test_registry_store_class(name, expected):
-    assert registry.classes[name] == expected
-
-
-@pytest.mark.parametrize(
-    "name,my_class,base_class",
-    [
-        ("SubclassDummy1", SubclassDummy1, DummyClass),
-        ("SubclassDummy2", SubclassDummy2, DummyClass),
-        ("NotSubclass", dict, dict),
-    ],
-)
-def test_dispatch(name, my_class, base_class):
-    assert registry.dispatch(name) == my_class and issubclass(registry.dispatch(name), base_class)
-
-
-def test_dispatch_exception():
-    with pytest.raises(ValueError):
-        registry_no_default.dispatch("NotSubclass")
+    @pytest.mark.parametrize(
+        "name,new_class", [("OrderedDict", defaultdict), ("defaultdict", Counter), ("Counter", defaultdict)]
+    )
+    def test_register_exception(self, name, new_class):
+        with pytest.raises(ValueError):
+            self.registry_dict.register(name)(new_class)
