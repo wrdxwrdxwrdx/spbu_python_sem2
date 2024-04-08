526b7538a9dc76ed5a97122793d19657a51ccc1f
Romanyuk Artem <117483924+wrdxwrdxwrdx@users.noreply.github.com>
2024-04-08 18:02:57 +0300

    Homework 1. Task 1. Romanyuk (#2)
    * Initial commit
    
    * fix: isort (import pytest)
    
    * fix: mypy
    
    * fix: pytest (import problem)
    
    * fix: removed base_class type checking from inner function
    
    * rename: class_module -> homework_1_task_1
    
    * rename: homeworks_1 -> homework_1
    
    * fix: rework Registry with Generic
    
    * fix: change TypeVar name

---
+++ src/homeworks/homework_1/homework_1_task_1.py
@@ -0,0 +1,55 @@
+from collections import Counter, OrderedDict, defaultdict
+from typing import Callable, Dict, Generic, Optional, Type, TypeVar
+
+T = TypeVar("T")
+
+USER_TEXT = (
+    "Registry with ParentClass 'dict', default=dict and Elements:\n"
+    "\t1)Counter\n"
+    "\t2)OrderedDict\n"
+    "\t3)defaultdict\n"
+    "Enter class name: "
+)
+
+
+class Registry(Generic[T]):
+    def __init__(self, default: Optional[Type[T]] = None) -> None:
+        self.default = default
+        self.classes: Dict[str, Type[T]] = dict()
+
+    def register(self, name: str) -> Callable[[Type[T]], Type[T]]:
+        """Decorator for adding a class with a name into the register"""
+
+        def inner(registered_class: Type[T]) -> Type[T]:
+            self.classes[name] = registered_class
+            return registered_class
+
+        if name in self.classes:
+            raise ValueError(f"{name} already in registry")
+        return inner
+
+    def dispatch(self, name: str) -> Type[T] | T:
+        """Get class with a name from the register"""
+        if name in self.classes:
+            return self.classes[name]
+        elif self.default is None:
+            raise ValueError(f"{name} was not registered")
+        else:
+            return self.default
+
+
+def main() -> None:
+    register = Registry[dict](default=dict)
+    register.register("Counter")(Counter)
+    register.register("OrderedDict")(OrderedDict)
+    register.register("defaultdict")(defaultdict)
+    user_input = input(USER_TEXT)
+    user_class = register.dispatch(user_input)
+    print("Return:", user_class)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as error:
+        print(error)
---
+++ tests/homeworks/homework_1/test_homework_1_task_1.py
@@ -0,0 +1,76 @@
+import pytest
+
+from src.homeworks.homework_1.homework_1_task_1 import *
+
+
+class TestRegistry:
+    class DummyClass:
+        pass
+
+    registry = Registry[DummyClass](default=dict)
+    registry_no_default = Registry[DummyClass]()
+    registry_dict = Registry[dict]()
+
+    for name, new_class in [("OrderedDict", OrderedDict), ("defaultdict", defaultdict), ("Counter", Counter)]:
+        registry_dict.register(name)(new_class)
+
+    @registry.register("SubclassDummy1")
+    @registry_no_default.register("SubclassDummy1")
+    class SubclassDummy1(DummyClass):
+        pass
+
+    @registry.register("SubclassDummy2")
+    @registry_no_default.register("SubclassDummy2")
+    class SubclassDummy2(DummyClass):
+        pass
+
+    @pytest.mark.parametrize(
+        "name,storage",
+        [
+            ("SubclassDummy1", registry),
+            ("SubclassDummy2", registry),
+            ("OrderedDict", registry_dict),
+            ("defaultdict", registry_dict),
+            ("Counter", registry_dict),
+        ],
+    )
+    def test_registry_store_name(self, name, storage):
+        assert name in storage.classes
+
+    @pytest.mark.parametrize("name,expected", [("SubclassDummy1", SubclassDummy1), ("SubclassDummy2", SubclassDummy2)])
+    def test_registry_store_class(self, name, expected):
+        assert self.registry.classes[name] == expected
+
+    @pytest.mark.parametrize(
+        "name,my_class,storage",
+        [
+            ("SubclassDummy1", SubclassDummy1, registry),
+            ("SubclassDummy2", SubclassDummy2, registry),
+            ("OrderedDict", OrderedDict, registry_dict),
+            ("defaultdict", defaultdict, registry_dict),
+            ("Counter", Counter, registry_dict),
+        ],
+    )
+    def test_dispatch(self, name, my_class, storage):
+        assert storage.dispatch(name) == my_class
+
+    @pytest.mark.parametrize(
+        "name,default",
+        [
+            ("NotInRegistry_1", dict),
+            ("NotInRegistry_2", dict),
+        ],
+    )
+    def test_default_dispatch(self, name, default):
+        assert self.registry.dispatch(name) == default
+
+    def test_dispatch_exception(self):
+        with pytest.raises(ValueError):
+            self.registry_no_default.dispatch("NotInRegistry")
+
+    @pytest.mark.parametrize(
+        "name,new_class", [("OrderedDict", defaultdict), ("defaultdict", Counter), ("Counter", defaultdict)]
+    )
+    def test_register_exception(self, name, new_class):
+        with pytest.raises(ValueError):
+            self.registry_dict.register(name)(new_class)
