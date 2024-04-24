9156840137e05efa07bbcc519eccf9d8c4ed2673
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2024-04-24 21:29:26 +0300

    fix: change 'float' to 'int'

--- src/homeworks/homework_2/storage.py
+++ src/homeworks/homework_2/storage.py
@@ -22,14 +22,14 @@ class Action:
 class AddToStart(Action):
     object_collection_type = MutableSequence
 
-    def __init__(self, number: float) -> None:
+    def __init__(self, number: int) -> None:
         self.number = number
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         object_collection.insert(0, self.number)
         return object_collection
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if len(object_collection) > 0:
             object_collection.pop(0)
             return object_collection
@@ -41,16 +41,16 @@ class DeleteFromStart(Action):
     object_collection_type = MutableSequence
 
     def __init__(self) -> None:
-        self.deleted_obj: Optional[float] = None
+        self.deleted_obj: Optional[int] = None
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if len(object_collection) > 0:
             self.deleted_obj = object_collection.pop(0)
             return object_collection
         else:
             raise EmptyStorageError()
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if self.deleted_obj is not None:
             object_collection.insert(0, self.deleted_obj)
             return object_collection
@@ -61,14 +61,14 @@ class DeleteFromStart(Action):
 class AddToEnd(Action):
     object_collection_type = MutableSequence
 
-    def __init__(self, number: float) -> None:
+    def __init__(self, number: int) -> None:
         self.number = number
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         object_collection.append(self.number)
         return object_collection
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if len(object_collection) > 0:
             object_collection.pop()
             return object_collection
@@ -80,16 +80,16 @@ class DeleteFromEnd(Action):
     object_collection_type = MutableSequence
 
     def __init__(self) -> None:
-        self.deleted_number: Optional[float] = None
+        self.deleted_number: Optional[int] = None
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if len(object_collection) > 0:
             self.deleted_number = object_collection.pop()
             return object_collection
         else:
             raise EmptyStorageError()
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if self.deleted_number is not None:
             object_collection.append(self.deleted_number)
             return object_collection
@@ -100,17 +100,17 @@ class DeleteFromEnd(Action):
 class AddToSet(Action):
     object_collection_type = Set
 
-    def __init__(self, number: float) -> None:
-        self.number: Optional[float] = number
+    def __init__(self, number: int) -> None:
+        self.number: Optional[int] = number
 
-    def action(self, object_collection: Set[float]) -> Set[float]:
+    def action(self, object_collection: Set[int]) -> Set[int]:
         if self.number in object_collection:
             self.number = None
         elif self.number is not None:
             object_collection.add(self.number)
         return object_collection
 
-    def inverse_action(self, object_collection: Set[float]) -> Set[float]:
+    def inverse_action(self, object_collection: Set[int]) -> Set[int]:
         if len(object_collection) > 0:
             if self.number is not None:
                 object_collection.remove(self.number)
@@ -122,17 +122,17 @@ class AddToSet(Action):
 class DeleteFromSet(Action):
     object_collection_type = Set
 
-    def __init__(self, number: float) -> None:
+    def __init__(self, number: int) -> None:
         self.number = number
 
-    def action(self, object_collection: Set[float]) -> Set[float]:
+    def action(self, object_collection: Set[int]) -> Set[int]:
         if self.number in object_collection:
             object_collection.remove(self.number)
             return object_collection
         else:
             raise ValueError(f"No element {self.number} in Storage")
 
-    def inverse_action(self, object_collection: Set[float]) -> Set[float]:
+    def inverse_action(self, object_collection: Set[int]) -> Set[int]:
         object_collection.add(self.number)
         return object_collection
 
@@ -144,14 +144,14 @@ class ChangeIndex(Action):
         self.first_index = first_index
         self.second_index = second_index
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if abs(self.first_index) < len(object_collection) and abs(self.second_index) < len(object_collection):
             object_collection.insert(self.second_index, object_collection.pop(self.first_index))
             return object_collection
         else:
             raise IndexError("Incorrect indexes")
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if abs(self.first_index) < len(object_collection) and abs(self.second_index) < len(object_collection):
             object_collection.insert(self.first_index, object_collection.pop(self.second_index))
             return object_collection
@@ -166,14 +166,14 @@ class AddValue(Action):
         self.index = index
         self.number = number
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if abs(self.index) < len(object_collection):
             object_collection[self.index] += self.number
             return object_collection
         else:
             raise IndexError("Incorrect indexes")
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if abs(self.index) < len(object_collection):
             object_collection[self.index] -= self.number
             return object_collection
@@ -184,10 +184,10 @@ class AddValue(Action):
 class Reverse(Action):
     object_collection_type = MutableSequence
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         return object_collection[::-1]
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         return object_collection[::-1]
 
 
@@ -198,16 +198,16 @@ class MultiplyValue(Action):
         self.index = index
         self.value = number
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if abs(self.index) < len(object_collection):
             object_collection[self.index] *= self.value
             return object_collection
         else:
             raise IndexError("Incorrect indexes")
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if abs(self.index) < len(object_collection):
-            object_collection[self.index] /= self.value
+            object_collection[self.index] //= self.value
             return object_collection
         else:
             raise IndexError("Incorrect indexes")
@@ -217,21 +217,21 @@ class SortAscending(Action):
     object_collection_type = MutableSequence
 
     def __init__(self) -> None:
-        self.unsorted_collection: Optional[MutableSequence[float]] = None
+        self.unsorted_collection: Optional[MutableSequence[int]] = None
 
     @staticmethod
-    def bubble_ascending(collection: MutableSequence[float]) -> MutableSequence[float]:
+    def bubble_ascending(collection: MutableSequence[int]) -> MutableSequence[int]:
         for i in range(0, len(collection) - 1):
             for j in range(0, len(collection) - 1 - i):
                 if collection[j] > collection[j + 1]:
                     collection[j], collection[j + 1] = collection[j + 1], collection[j]
         return collection
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         self.unsorted_collection = copy(object_collection)
         return self.bubble_ascending(object_collection)
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if self.unsorted_collection is not None:
             return self.unsorted_collection
         else:
@@ -242,21 +242,21 @@ class SortDescending(Action):
     object_collection_type = MutableSequence
 
     def __init__(self) -> None:
-        self.unsorted_collection: Optional[MutableSequence[float]] = None
+        self.unsorted_collection: Optional[MutableSequence[int]] = None
 
     @staticmethod
-    def bubble_descending(collection: MutableSequence[float]) -> MutableSequence[float]:
+    def bubble_descending(collection: MutableSequence[int]) -> MutableSequence[int]:
         for i in range(0, len(collection) - 1):
             for j in range(0, len(collection) - 1 - i):
                 if collection[j] < collection[j + 1]:
                     collection[j], collection[j + 1] = collection[j + 1], collection[j]
         return collection
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         self.unsorted_collection = copy(object_collection)
         return self.bubble_descending(object_collection)
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if self.unsorted_collection is not None:
             return self.unsorted_collection
         else:
@@ -267,14 +267,14 @@ class Shuffle(Action):
     object_collection_type = MutableSequence
 
     def __init__(self) -> None:
-        self.unshuffled_collection: Optional[MutableSequence[float]] = None
+        self.unshuffled_collection: Optional[MutableSequence[int]] = None
 
-    def action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         self.unshuffled_collection = copy(object_collection)
         random.shuffle(object_collection)
         return object_collection
 
-    def inverse_action(self, object_collection: MutableSequence[float]) -> MutableSequence[float]:
+    def inverse_action(self, object_collection: MutableSequence[int]) -> MutableSequence[int]:
         if self.unshuffled_collection is not None:
             return self.unshuffled_collection
         else:
