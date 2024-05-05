8b77798ae30e8c6cd0ed404e7b98282d516c8212
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2024-05-05 13:39:04 +0300

    fix: rework __setitem__

--- src/homeworks/homework_1/homework_1_task_2.py
+++ src/homeworks/homework_1/homework_1_task_2.py
@@ -1,6 +1,6 @@
 import random
 from collections.abc import MutableMapping
-from typing import Generic, Iterable, Iterator, Optional, Set, Tuple, TypeVar
+from typing import Generic, Iterable, Iterator, Optional, Set, Tuple, TypeVar, Sequence
 
 K = TypeVar("K")
 V = TypeVar("V")
@@ -20,28 +20,21 @@ class Node(Generic[K, V]):
         self.right = right
         self.key = key
         self.value = value
-        if priority:
-            self.priority = priority
-        else:
-            self.priority = random.random()
+        if priority is None:
+            priority = random.random()
+        self.priority = priority
 
     def __str__(self) -> str:
-        if self:
-            return f"[<{self.key}, {self.value}>, left={self.left}, right={self.right}]"
-        else:
-            return "None"
+        return f"[<{self.key}, {self.value}>, left={self.left}, right={self.right}]"
 
     def __repr__(self) -> str:
-        if self:
-            return f"[<{self.key}, {self.value}, {self.priority}>, left={self.left.__repr__()}, right={self.right.__repr__()}]"
-        else:
-            return "None"
+        return f"[<{self.key}, {self.value}, {self.priority}>, left={repr(self.left)}, right={repr(self.right)}]"
 
 
-class Treap(MutableMapping):
-    def __init__(self, root: Node = None, size: int = 0):
-        self.root = root
-        self.size = size
+class Treap(MutableMapping, Generic[K, V]):
+    def __init__(self) -> None:
+        self.root: Optional[Node] = None
+        self.size: int = 0
 
     @staticmethod
     def merge_node(left_node: Optional[Node], right_node: Optional[Node]) -> Optional[Node]:
@@ -98,46 +91,24 @@ class Treap(MutableMapping):
         return getitem(self.root, key)
 
     def __iter__(self) -> Iterator[K]:
-        def iterate(root: Optional[Node], output: Set[K]) -> Set[K]:
+        def iterate(root: Optional[Node]) -> Iterator[K]:
             if root is not None:
-                output.add(root.key)
-                output = iterate(root.left, output)
-                output = iterate(root.right, output)
-            return output
+                yield from iterate(root.left)
+                yield root.key
+                yield from iterate(root.right)
 
-        return iter(iterate(self.root, set()))
+        return iterate(self.root)
 
     def __len__(self) -> int:
         return self.size
 
     def __setitem__(self, key: K, value: V) -> None:
-        def update(__m: Iterable[Tuple[K, V]]) -> None:
-            def _update(root: Optional[Node], key: K, value: V) -> Optional[Node]:
-                if root is None:
-                    return root
-                if key == root.key:
-                    root.value = value
-                    return root
-                if root is not None and key < root.key:
-                    root.left = _update(root.left, key, value)
-                    return root
-                if root is not None and key > root.key:
-                    root.right = _update(root.right, key, value)
-                    return root
-
-            for key, value in __m:
-                self.root = _update(self.root, key, value)
-
-        if self.root:
-            if key in self:
-                update(((key, value),))
-            left_treap, right_treap = self.split_node(self.root, key)
-            new_element: Node = Node(key, value)
-            left_treap = self.merge_node(left_treap, new_element)
-            self.root = self.merge_node(left_treap, right_treap)
-        else:
-            self.root = Node(key, value)
-
+        if key in self:
+            del self[key]
+        left_node, right_node = self.split_node(self.root, key)
+        new_node = Node(key, value)
+        left_node = self.merge_node(left_node, new_node)
+        self.root = self.merge_node(left_node, right_node)
         self.size += 1
 
     def __str__(self) -> str:
@@ -150,6 +121,6 @@ class Treap(MutableMapping):
     def __repr__(self) -> str:
         root = self.root
         if root:
-            return f"[<{root.key}, {root.value}, {root.priority}>, left={root.left.__repr__()}, right={root.right.__repr__()}]"
+            return f"[<{root.key}, {root.value}, {root.priority}>, left={repr(root.left)}, right={repr(root.right)}]"
         else:
             return "None"
